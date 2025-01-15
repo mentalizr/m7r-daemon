0e4cc9c8a417e9b5ab88ceafa8d3702b81efad4d
Arthur Picht <picht@arthurpicht.de>
2025-01-15 16:37:54 +0100

    systemd configuration

--- src/main/java/org/mentalizr/daemon/appInit/ApplicationInitialization.java
+++ src/main/java/org/mentalizr/daemon/appInit/ApplicationInitialization.java
@@ -2,6 +2,7 @@ package org.mentalizr.daemon.appInit;
 
 import ch.qos.logback.classic.Level;
 import de.arthurpicht.utils.logging.LoggerInit;
+import org.mentalizr.commons.paths.host.hostDir.M7rDaemonConfigDir;
 import org.mentalizr.commons.paths.host.hostDir.M7rHostLogDir;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class ApplicationInitialization {
     public static void execute() {
         createLogDir();
         initLogging();
+        createDaemonConfigDir();
     }
 
     private static void createLogDir() throws ApplicationInitializationException {
@@ -31,6 +33,19 @@ public class ApplicationInitialization {
         M7rHostLogDir m7rHostLogDir = new M7rHostLogDir();
         Path logFile = m7rHostLogDir.asPath().resolve("m7r-daemon.log");
         LoggerInit.consoleAndFile(logFile, Level.DEBUG, Level.OFF);
+    }
+
+    private static void createDaemonConfigDir() {
+        M7rDaemonConfigDir m7rDaemonConfigDir = new M7rDaemonConfigDir();
+        if (!m7rDaemonConfigDir.exists()) {
+            try {
+                m7rDaemonConfigDir.create();
+            } catch (IOException e) {
+                throw new ApplicationInitializationException(
+                        "Application initialization failed. Could not create directory ["
+                                + m7rDaemonConfigDir.toAbsolutePathString() + "]");
+            }
+        }
     }
 
 }
